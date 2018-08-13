4d17f364ab194ad3bc0e9c07bb9c13eab50067f1
Jérôme Laban <jerome.laban@nventive.com>
2018-08-13 12:37:51 -0400

    Add stubbed ToggleSwitchTemplateSettings

---
+++ src/Uno.UI/UI/Xaml/Controls/Primitives/ToggleSwitchTemplateSettings.cs
@@ -0,0 +1,80 @@
+#pragma warning disable 108 // new keyword hiding
+#pragma warning disable 114 // new keyword hiding
+namespace Windows.UI.Xaml.Controls.Primitives
+{
+	public  partial class ToggleSwitchTemplateSettings : global::Windows.UI.Xaml.DependencyObject
+	{
+		[global::Uno.NotImplemented]
+		public  double CurtainCurrentToOffOffset
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.ToggleSwitchTemplateSettings", "CurtainCurrentToOffOffset");
+				return 0;
+			}
+		}
+
+		public  double CurtainCurrentToOnOffset
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.ToggleSwitchTemplateSettings", "CurtainCurrentToOnOffset");
+				return 0;
+			}
+		}
+
+		public  double CurtainOffToOnOffset
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.ToggleSwitchTemplateSettings", "CurtainOffToOnOffset");
+				return 0;
+			}
+		}
+
+		public  double CurtainOnToOffOffset
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.ToggleSwitchTemplateSettings", "CurtainOnToOffOffset");
+				return 0;
+			}
+		}
+
+		public  double KnobCurrentToOffOffset
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.ToggleSwitchTemplateSettings", "KnobCurrentToOffOffset");
+				return 0;
+			}
+		}
+
+		public  double KnobCurrentToOnOffset
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.ToggleSwitchTemplateSettings", "KnobCurrentToOnOffset");
+				return 0;
+			}
+		}
+
+		public  double KnobOffToOnOffset
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.ToggleSwitchTemplateSettings", "KnobOffToOnOffset");
+				return 0;
+			}
+		}
+
+		public  double KnobOnToOffOffset
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.ToggleSwitchTemplateSettings", "KnobOnToOffOffset");
+				return 0;
+			}
+		}
+	}
+}
